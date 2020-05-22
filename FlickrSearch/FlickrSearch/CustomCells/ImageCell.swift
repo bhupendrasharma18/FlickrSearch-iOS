8ff5cfe25b6d41c7fcae3f75acf1940bd485f18b
Bhupendra <bhupendrasharma18@gmail.com>
2020-05-22 20:12:38 +0530

    CollectionView added

---
+++ FlickrSearch/FlickrSearch/CustomCells/ImageCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCell.swift
+//  FlickrSearch
+//
+//  Created by Bhupendra Sharma on 22/05/20.
+//  Copyright © 2020 Bhupendra. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        imgView.backgroundColor = .blue
+    }
+
+}
--- FlickrSearch/FlickrSearch/ViewController.swift
+++ FlickrSearch/FlickrSearch/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    let identifier = "ImageCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        collectionView.register(UINib.init(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: identifier)
     }
+}
 
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ImageCell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! ImageCell
+        return cell
+    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+    }
 }
 
